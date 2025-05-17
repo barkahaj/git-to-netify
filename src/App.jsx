a47b17c2b29e287eb865d7ebf1dd55a46b18750a
Barkah Aji <barkah993@gmail.com>
2025-05-17 12:12:18 +0700

    sending barkah image

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import barkahLogo from './assets/profile-picture.png'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -10,7 +10,7 @@ function App() {
     <>
       <div>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={barkahLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite - github - netify integration</h1>
